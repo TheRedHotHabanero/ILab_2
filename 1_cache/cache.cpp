073cd29699301f3fbcb81b6bfb6fce118c33c493
theredhothabanero <lkrn10001@gmail.com>
2021-09-29 00:15:11 +0300

    better code, broken tests

--- 1_cache/cache.cpp
+++ 1_cache/cache.cpp
@@ -1,18 +1,35 @@
+#include <cassert>
+
 #include "cache.h"
+
+int finding_hits(size_t size_)
+{
+  int hits = 0;
+  caches_lfu::CacheLFU<int> lfu{size_};
+  for (int curr = 0; curr < size_; ++curr) 
+  {
+    int new_key;
+    std::cin >> new_key;
+    assert(std::cin.good());
+
+    if (lfu.process_elem(new_key) == true)
+      hits++;
+  }
+  return hits;
+}
 
 int main()
 {
-
   size_t cache_size = 0;
   int el_quantity = 0;
   int hits = 0;
-  cin >> cache_size;
-  cin >> el_quantity;
-  assert(cin.good());
+  std::cin >> cache_size;
+  std::cin >> el_quantity;
+  assert(std::cin.good());
 
-  caches_lfu::CacheLFU lfu{cache_size};
-  hits = lfu.finding_hits(el_quantity);
-  cout << hits << endl;
+  caches_lfu::CacheLFU<int> lfu{cache_size};
+  hits = finding_hits(el_quantity);
+  std::cout << hits << std::endl;
 
   return 0;
 }
