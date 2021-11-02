c2fb220c0595b6ba80035a3da0bfd1a7e51b001e
theredhothabanero <lkrn10001@gmail.com>
2021-11-02 16:47:16 +0300

    new function order, better naming, minor fixes

--- 1_cache/cache.h
+++ 1_cache/cache.h
@@ -2,7 +2,6 @@
 
 #include <unordered_map>
 #include <list>
-#include <cassert>
 
 namespace LFU
 {
@@ -28,8 +27,8 @@ namespace LFU
   struct FreqElem
   {
     int hits_;
-    list<NodeElem<KeyT>> NodeList;
-    FreqElem(int hits): hits_(hits), NodeList() {}
+    list<NodeElem<KeyT>> node_list_;
+    FreqElem(int hits): hits_(hits), node_list_() {}
   };
 
   template <typename KeyT>
@@ -44,36 +43,36 @@ namespace LFU
       HashTable             hash_table_;
       list<FreqElem<KeyT>>  freq_list_;
 
+      void adding_freq(KeyT key)
+      {
+        if (freq_list_.empty() || freq_list_.front().hits_ != 0)
+          freq_list_.push_front(FreqElem<KeyT>(0));
+        freq_list_.front().node_list_.push_front(NodeElem<KeyT>(key, freq_list_.begin()));
+      }
+
+      void delete_min_freq()
+      {
+        hash_table_.erase(freq_list_.front().node_list_.front().key_);
+        freq_list_.front().node_list_.pop_front();
+
+        if (freq_list_.front().node_list_.empty())
+          freq_list_.pop_front();
+      }
+
       void adding_key(KeyT key)
       {
         if (hash_table_.size() >= size_)
           delete_min_freq();
         adding_freq(key);
-        hash_table_[key] = freq_list_.front().NodeList.begin();
-      }
-
-      void adding_freq(KeyT key)
-      {
-        if (freq_list_.empty() || freq_list_.front().hits_ != 0)
-          freq_list_.push_front(FreqElem<KeyT>(0));
-        freq_list_.front().NodeList.push_front(NodeElem<KeyT>(key, freq_list_.begin()));
-      }
-
-      void delete_min_freq()
-      {
-        hash_table_.erase(freq_list_.front().NodeList.front().key_);
-        freq_list_.front().NodeList.pop_front();
-
-        if (freq_list_.front().NodeList.empty())
-          freq_list_.pop_front();
+        hash_table_[key] = freq_list_.front().node_list_.begin();
       }
 
       void tying_node(NodeIt &node, FreqIt<KeyT> &current, FreqIt<KeyT> &destination)
       {
         hash_table_.erase(node->key_);
-        hash_table_[node->key_] = destination->NodeList.begin();
-        current->NodeList.erase(node);
-        if (current->NodeList.empty())
+        hash_table_[node->key_] = destination->node_list_.begin();
+        current->node_list_.erase(node);
+        if (current->node_list_.empty())
           freq_list_.erase(current);
       }
 
@@ -83,15 +82,15 @@ namespace LFU
         FreqIt<KeyT> node_head_p(++node_head);
         node_head--;
 
-        if (node_head_p != freq_list_.end() && node_head->hits_ + 1 == node_head_p->hits_)
+        if (node_head_p != freq_list_.end() && (node_head->hits_ + 1) == node_head_p->hits_)
         {
-          node_head_p->NodeList.push_front(NodeElem<KeyT>(new_node_it->key_, node_head_p));
+          node_head_p->node_list_.push_front(NodeElem<KeyT>(new_node_it->key_, node_head_p));
           tying_node(new_node_it, node_head, node_head_p);
           return;
         }
 
         FreqIt<KeyT> new_head = freq_list_.insert(node_head_p, FreqElem<KeyT>(node_head->hits_ + 1));
-        new_head->NodeList.push_front(NodeElem<KeyT>(new_node_it->key_, new_head));
+        new_head->node_list_.push_front(NodeElem<KeyT>(new_node_it->key_, new_head));
         tying_node(new_node_it, node_head, new_head);
       }
 
@@ -109,5 +108,5 @@ namespace LFU
         adding_key(key);
         return false;
       }
-  };
-}; //namespace LFU
+  };  // class LFUCache
+};    //namespace LFU
