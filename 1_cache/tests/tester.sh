794285e05e0ecd9efb976fd391c8c951c14db378
theredhothabanero <lkrn10001@gmail.com>
2021-11-02 16:33:19 +0300

    better tester.sh

--- 1_cache/tests/tester.sh
+++ 1_cache/tests/tester.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 files=./*.dat
+status=true
 
 for fname in $files
 do
@@ -10,10 +11,18 @@ do
   diff $fname.res $bname.ans
   if (($? != 0))
   then
-    echo
-    echo "FAILED $fname"
-    echo '-------------------------------------'
+    echo " ------------------------------------- "
+    echo "❘           FAILED $fname""        ❘"
+    echo " ------------------------------------- "
+    status=false
   fi
 
   rm $fname.res
 done
+
+if $status
+then
+  echo " ------------------------------------- "
+  echo "❘               PASSED               ❘"
+  echo " ------------------------------------- "
+fi
