5326f6fada2b53764fb0a3334923a3efed5d07a7
theredhothabanero <lkrn10001@gmail.com>
2021-11-02 14:41:26 +0300

    minor fixes

--- 1_cache/cache.cpp
+++ 1_cache/cache.cpp
@@ -5,24 +5,24 @@
 int finding_hits(size_t size, size_t el_quantity)
 {
   int hits = 0;
-  caches_lfu::LFUCache<int> lfu{size};
-  for (size_t curr = 0; curr < el_quantity; ++curr) 
+  LFU::LFUCache<int> lfu{size};
+  for (size_t curr = 0; curr < el_quantity; ++curr)
   {
     int new_key;
     std::cin >> new_key;
     assert(std::cin.good());
 
     if (lfu.process_elem(new_key) == true)
-      hits++;  
+      hits++;
   }
   return hits;
 }
 
 int main()
 {
-  size_t cache_size = 0;
-  int el_quantity = 0;
-  int hits = 0;
+  int     hits        = 0;
+  int     el_quantity = 0;
+  size_t  cache_size  = 0;
   std::cin >> cache_size;
   std::cin >> el_quantity;
   assert(std::cin.good());
--- 1_cache/cache.h
+++ 1_cache/cache.h
@@ -5,128 +5,115 @@
 #include <cassert>
 #include <limits>
 
-namespace caches_lfu 
+namespace LFU
 {
   using std::unordered_map;
   using std::list;
-  
-  template <typename ValueT>
+
+  template <typename KeyT>
   struct FreqElem;
 
-  template <typename ValueT>
-  using FreqIt = typename list<FreqElem<ValueT>>::iterator;
+  template <typename KeyT>
+  using FreqIt  = typename list<FreqElem<KeyT>>::iterator;
+  using FreqT   = unsigned long long;
 
-  template <typename ValueT>
+  template <typename KeyT>
   struct NodeElem
   {
-    ValueT value_;
-    FreqIt<ValueT> head_;
-    NodeElem(ValueT value, FreqIt<ValueT> head) : value_(value), head_(head) {}
+    KeyT value_;
+    FreqIt<KeyT> head_;
+    NodeElem(KeyT value, FreqIt<KeyT> head): value_(value), head_(head) {}
   };
-  
-  template<typename ValueT>
+
+  template<typename KeyT>
   struct FreqElem
   {
-    std::list<NodeElem<ValueT>> NodeList;
-    const int hits_;
+    int hits_;
+    list<NodeElem<KeyT>> NodeList;
     FreqElem(int hits): NodeList(), hits_(hits) {}
   };
 
-  using FreqT = unsigned long long;
   template <typename KeyT>
   class LFUCache
   {
     private:
-    
-      size_t capacity_;
-      list<FreqElem<KeyT>> FreqList;
-
       using NodeIt    = typename list<NodeElem<KeyT>>::iterator;
       using HashTable = unordered_map<KeyT, NodeIt>;
       using HashIt    = typename HashTable::iterator;
 
-      HashTable hash_table;
-    public:
+      size_t                size_;
+      HashTable             hash_table_;
+      list<FreqElem<KeyT>>  freq_list_;
 
-      LFUCache(size_t capacity): capacity_(capacity), hash_table(), FreqList() {}
-  
+      KeyT Hash(KeyT value) const
+      { return value; }
+
+      void funding_hits(NodeIt new_node_it)
+      {
+        FreqIt<KeyT> node_head(new_node_it->head_);
+        FreqIt<KeyT> node_head_p(++node_head);
+        --node_head;
+
+        if (node_head_p != freq_list_.end() && node_head->hits_ + 1 == node_head_p->hits_)
+        {
+          node_head_p->NodeList.push_front(NodeElem<KeyT>(new_node_it->value_, node_head_p));
+          tying_node(new_node_it, node_head, node_head_p);
+          return;
+        }
+
+        FreqIt<KeyT> new_head = freq_list_.insert(node_head_p, FreqElem<KeyT>(node_head->hits_ + 1));
+        new_head->NodeList.push_front(NodeElem<KeyT>(new_node_it->value_, new_head));
+        tying_node(new_node_it, node_head, new_head);
+      }
+
+      void tying_node(NodeIt &node, FreqIt<KeyT> &current, FreqIt<KeyT> &destination)
+      {
+        hash_table_.erase(node->value_);
+        hash_table_[node->value_] = destination->NodeList.begin();
+        current->NodeList.erase(node);
+        if (current->NodeList.empty())
+          freq_list_.erase(current);
+      }
+
+      void adding_value(KeyT value)
+      {
+        if (hash_table_.size() >= size_)
+          delete_min_freq();
+        adding_freq(value);
+        hash_table_[value] = freq_list_.front().NodeList.begin();
+      }
+
+      void adding_freq(KeyT value)
+      {
+        if (freq_list_.empty() || freq_list_.front().hits_ != 0)
+          freq_list_.push_front(FreqElem<KeyT>(0));
+        freq_list_.front().NodeList.push_front(NodeElem<KeyT>(value, freq_list_.begin()));
+      }
+
+      void delete_min_freq(void)
+      {
+        hash_table_.erase(freq_list_.front().NodeList.front().value_);
+        freq_list_.front().NodeList.pop_front();
+
+        if (freq_list_.front().NodeList.empty())
+          freq_list_.pop_front();
+      }
+
+    public:
+      LFUCache(size_t size): size_(size) {}
+
       bool process_elem(const KeyT &value)
       {
-        KeyT value_hash = Hash(value);
-        auto value_list_it = hash_table.find(value_hash);
+        KeyT value_hash     = Hash(value);
+        auto value_list_it  = hash_table_.find(value_hash);
 
-        if (value_list_it != hash_table.end())
+        if (value_list_it != hash_table_.end())
         {
           funding_hits(value_list_it->second);
           return true;
         }
-        AddCache_(value);
+        adding_value(value);
         return false;
       }
-
-    private:
-      template <typename ValueT>
-      static ValueT Hash( ValueT value )
-      { return value; }
-
-      //Increment amount of hits in value function
-      void funding_hits(NodeIt new_node_it)
-      {
-        FreqIt<KeyT> n_head(new_node_it->head_); ////////////////////////////////////////
-        FreqIt<KeyT> n_head_p1(++n_head);       ///////////////////////////////////////
-        --n_head;                               ///////////////////////////////////////
-
-        if (n_head_p1 != FreqList.end() && n_head->hits_ + 1 == n_head_p1->hits_)
-        {
-          n_head_p1->NodeList.push_front(NodeElem<KeyT>(new_node_it->value_, n_head_p1));
-          ReTie_(new_node_it, n_head, n_head_p1);
-          return;
-        }
-
-        // Add list with new hits amount
-        FreqIt<KeyT> new_head = FreqList.insert(n_head_p1, FreqElem<KeyT>(n_head->hits_ + 1));
-        // push node to new freq list
-        new_head->NodeList.push_front(NodeElem<KeyT>(new_node_it->value_, new_head));
-        ReTie_(new_node_it, n_head, new_head);
-      }
-
-      // Retie a node function
-      // param node reference to node iterator for retie
-      // param current reference to iterator to element of Freq_list, where node is located
-      // param destination reference to iterator to element of Freq_list, where node will be located
-      void ReTie_(NodeIt &node, FreqIt<KeyT> &current, FreqIt<KeyT> &destination)
-      {
-        hash_table.erase(node->value_);
-        hash_table[node->value_] = destination->NodeList.begin();
-        current->NodeList.erase(node);
-        if (current->NodeList.empty())
-          FreqList.erase(current);
-      }
-
-      //Add new value to cache function
-      void AddCache_(KeyT value)
-      {
-        if (hash_table.size() >= capacity_)
-          DelMin_();
-        AddFreq_(value);
-        hash_table[value] = FreqList.front().NodeList.begin();
-      }
-
-      void DelMin_(void)
-      {
-        hash_table.erase(FreqList.front().NodeList.front().value_);
-        FreqList.front().NodeList.pop_front();
-
-        if (FreqList.front().NodeList.empty())
-          FreqList.pop_front();
-      }
-
-      //Add new Frequency to Freq_list function
-      void AddFreq_(KeyT value)
-      {
-        if (FreqList.empty() || FreqList.front().hits_ != 0)
-          FreqList.push_front(FreqElem<KeyT>(0));
-        FreqList.front().NodeList.push_front(NodeElem<KeyT>(value, FreqList.begin()));
-      }
-  };    
-};  
+  };
+}; //namespace LFU
